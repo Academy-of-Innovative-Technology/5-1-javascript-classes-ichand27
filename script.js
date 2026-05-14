9fd631bedc7e005a6c74a2156e21be714a8a7e5a
ichand27 <ichand27@aoiths.org>
2026-05-14 12:41:09 -0400

    JS CONT.

---
+++ script.js
@@ -0,0 +1,43 @@
+class mathFunctions {
+
+     constructor(a,b) {
+     this.a = a
+     this.b = b
+     }
+
+
+     add()  {
+          return this.a +  this.b;
+     }
+
+     subtract()  {
+          return this.a - this.b;
+     }
+
+     multiply()  {
+          return this.a * this.b;
+     }
+
+     divide()  {
+          return this.a / this.b;
+     }
+}
+
+let iwkms = new mathFunctions(666, 333);
+
+console.log(iwkms.add(), iwkms.subtract(), iwkms.multiply(), iwkms.divide());   
+
+class greetings {
+
+     constructor(){}
+
+english(){}
+
+spanish(){}
+
+french(){}
+
+arabic(){}
+
+}
+
