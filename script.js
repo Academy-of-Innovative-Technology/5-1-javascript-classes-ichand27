8e58568babe46f7e3123d8d20d7fe8d0a1c30fb4
ichand27 <ichand27@aoiths.org>
2026-05-15 12:41:08 -0400

    DOne

--- script.js
+++ script.js
@@ -29,15 +29,37 @@ console.log(iwkms.add(), iwkms.subtract(), iwkms.multiply(), iwkms.divide());
 
 class greetings {
 
-     constructor(){}
+     constructor(name){
+          this.name = name;
+     }
 
-english(){}
+english(){
+     return  "Hello " + this.name
+}
 
-spanish(){}
+spanish(){
+     return  "Hola " + this.name
+}
 
-french(){}
+french(){
+     return  "Bonjour " + this.name
+}
 
-arabic(){}
+arabic(){
+     return   "Marhaba " +this.name
+}
+
+//Bonus//
+mandarinChinese(){
+     return   "Nǐ hǎo " +this.name
+}
 
 }
 
+let greet = new greetings('Samaira');
+
+console.log(greet.english());
+console.log(greet.spanish());
+console.log(greet.french());
+console.log(greet.arabic());
+console.log(greet.mandarinChinese());
